2ed6ec51c446234e9bbacc671d4821abaaac62f2
ELowis <game.playerle@ymail.com>
2018-03-11 20:09:41 +0100

    Used scikit-learn mean_squared_error & pandas for more exact error

--- Task 0/evaluate.py
+++ Task 0/evaluate.py
@@ -3,25 +3,29 @@
 # To do this, we make predictions on the training
 # data and compare them to the given output. 
 # The resulting RMSE should be 0, since there is no noise. 
-import csv
-
+import pandas
+from sklearn.metrics import mean_squared_error
 
 train_file = 'train.csv'
 result_file = 'train_results.csv'
 
-with open(train_file, 'r') as csv_train:
-    with open(result_file, 'r') as csv_results:
-        train_reader = csv.DictReader(csv_train)
-        res_reader = csv.DictReader(csv_results)
-        print('Comparing y columns from ' + train_file + ' and ' + result_file + '...')
-        n = 0
-        RMSE = 0
-        for train_row in train_reader:
-            for res_row in res_reader:
-                if train_row['Id'] == res_row['Id']:
-                    y = float(train_row['y'])
-                    y_pred = float(res_row['y'])
-                    RMSE = RMSE + (y - y_pred) ** 2.0
-                    n = n + 1
-        RMSE = (RMSE / n) ** 0.5
-        print('Root Mean Square Error: ' + str(RMSE))
+colnames = ['Id', 'y', 'x1', 'x2', 'x3', 'x4', 'x5', 'x6', 'x7', 'x8', 'x9', 'x10']
+csv_train = pandas.read_csv(train_file, names=colnames)
+csv_results = pandas.read_csv(result_file, names=['Id', 'y'])
+
+train_ids = csv_train.Id.tolist()
+result_ids = csv_results.Id.tolist()
+#print(train_ids)
+#print(result_ids)
+if (len(train_ids) == len(result_ids)):
+    print('Comparing y columns from ' + train_file + ' and ' + result_file + '...')
+    y = csv_train.y.tolist()
+    y.pop(0)
+    y = [float(i) for i in y]
+    y_pred = csv_results.y.tolist()
+    y_pred.pop(0)
+    y_pred = [float(i) for i in y_pred]
+    RMSE = mean_squared_error(y, y_pred) ** 0.5
+    print('Root Mean Square Error: ' + str(RMSE))
+else:
+    print('Ids do not match: ' + str([e for e in train_ids if e not in result_ids]))
