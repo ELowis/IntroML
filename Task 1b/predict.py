aaebb85984c904e2892e3691de03a1738d9b7c35
Christian Knabenhans <knabenhc@student.ethz.ch>
2018-03-19 14:11:22 +0100

    Added weigth for constant data column

--- Task 1b/predict.py
+++ Task 1b/predict.py
@@ -21,7 +21,7 @@ exponential = np.exp(X)
 cosine = np.cos(X)
 features = np.concatenate([linear, quadratic, exponential, cosine], axis=1)
 
-lambdas = np.array([0.1, 1, 10, 100, 1000])
+lambdas = np.array([0.1, 1, 10, 100])#, 1000])
 regressions = np.array([])
 errors = np.array([])
 
@@ -29,7 +29,7 @@ for l in lambdas:
     print('Performing cross-validation for lambda = ' + str(l) + ' ...')
     regression = svm.SVR(kernel='linear', C=l)
     # Use MSE instead of RMSE since only comparison matters
-    scores = model_selection.cross_val_score(regression, X, y, cv=3, scoring='neg_mean_squared_error')
+    scores = model_selection.cross_val_score(regression, features, y, cv=3, scoring='neg_mean_squared_error')
     regressions = np.append(regressions, regression)
     RMSE = (-np.mean(scores)) ** 0.5
 
@@ -42,9 +42,12 @@ for i in range(errors.size):
     if errors[i] == min_error:
         best_regr = regressions[i]
 
-best_regr.fit(X, y)
+best_regr.fit(features, y)
         
 with open('res.csv', 'w+') as res_file:
-    res = "\n".join(map(str, best_regr.coef_.tolist()))
-    print(res)
-    res_file.write(res)
+    intercept = best_regr.intercept_[0]
+    l = best_regr.coef_[0].tolist()
+    m = map(str, l)
+    sl = list(m)
+    weights = '\n'.join(sl + [str(intercept)])
+    res_file.write(weights)
